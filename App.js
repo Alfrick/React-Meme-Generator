48277e3b9ce2f09391043de9a70b83930ffc5496
Alfrick Opidi <alfo.opidi85@gmail.com>
2019-07-15 14:02:38 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/Headers'
+import MemeGenerator from './components/MemeGenerator'
+
+
+function App(){
+  
+  return(
+    <div>
+    <Header />
+    <MemeGenerator />
+    </div>
+
+    )
+}
+
+export default App
