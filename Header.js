c1227ec32a460528c818e1989efef39fe30ac83a
Alfrick Opidi <alfo.opidi85@gmail.com>
2019-07-15 14:03:44 +0300

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header(){
+	return(
+		<div>
+
+		<header>
+			<img src="https://images.unsplash.com/photo-1521249664898-864e6c1b6d5c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" alt="coming"/>
+			<h3>Meme Generator </h3>
+		</header>
+
+		</div>)
+
+}
+
+export default Header
